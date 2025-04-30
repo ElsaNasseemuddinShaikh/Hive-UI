177715ef16ae465ef7b0d6fe6ce8507c408dd6ef
Elsa Shaikh <elsa.shaikh@mightyfurniture.co.uk>
2025-04-30 18:15:48 +0500

    updated vector image

--- src/component/Banner.tsx
+++ src/component/Banner.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import CustomButton from "./Ui/ButtonComponent";
 import group from "../../public/assets/Group.png";
+import ai_logo from "../../public/assets/ai logo.png";
 import Image from "next/image";
 
 const BannerSection = () => {
@@ -35,18 +36,34 @@ const BannerSection = () => {
         </div>
       </div>
 
-      {/* Image Section */}
-      <div className="w-full sm:w-[500px] md:w-[600px] lg:w-[716.87px] h-[300px] sm:h-[400px] md:h-[500px] lg:h-[710px] flex items-center justify-center relative overflow-hidden">
+    
+     {/* Image Section */}
+     <div className="w-full sm:w-[500px] md:w-[600px] lg:w-[716.87px] h-[300px] sm:h-[400px] md:h-[500px] lg:h-[710px] flex items-center justify-center relative overflow-hidden">
         <div className="relative w-full h-full">
+          {/* Background circuit image */}
           <Image
-            src={group}
+            src={group || "/placeholder.svg"}
             alt="Circuit Graphic"
             layout="fill"
             objectFit="contain"
             className="pointer-events-none"
           />
+
+          {/* Centered AI logo */}
+          <div className="absolute inset-0 flex items-center justify-center z-10 transform translate-y-[-0%] translate-x-[3%]">
+            <div className="relative w-[300px] h-[700px] xs:w-[350px] xs:h-[350px] sm:w-[450px] sm:h-[450px] md:w-[500px] md:h-[500px] lg:w-[600px] lg:h-[430px]">
+              <Image
+                src={ai_logo || "/placeholder.svg"}
+                alt="AI Logo"
+                layout="fill"
+                objectFit="contain"
+                className="pointer-events-none"
+              />
+            </div>
+          </div>
         </div>
       </div>
+
     </section>
   );
 };
